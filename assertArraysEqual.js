232748b713bb0de83da9691c00660e2490316a67
pochiman <pochiman@yahoo.com>
2021-11-17 20:06:51 -0500

    initial commit

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -23,9 +23,9 @@ module.exports = eqArrays;
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`👍  Assertion Passed: ${array1} === ${array2}`);
+    console.log(`👍 Assertion Passed: ${array1} === ${array2}`);
   } else {
-    console.log(`👎  Assertion Failed: ${array1} !== ${array2}`);
+    console.log(`👎 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
