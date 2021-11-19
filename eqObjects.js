84cd9e72c6ddccbcba7d33286f6918651fc7e1ff
pochiman <pochiman@yahoo.com>
2021-11-18 22:56:21 -0500

    initial commit

--- eqObjects.js
+++ eqObjects.js
@@ -1,5 +1,38 @@
-const eqArrays = require('./eqArrays');
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
+module.exports = assertEqual;
+
+
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] && (!Array.isArray(array1[i]) || !Array.isArray(array2[i]))) {
+      return false;
+    }
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+module.exports = eqArrays;
+
+
+// FUNCTION IMPLEMENTATION
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) return false;
 
@@ -17,3 +50,19 @@ const eqObjects = function(object1, object2) {
 };
 
 module.exports = eqObjects;
+
+// TEST CODE - Primitives As Values
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc)); // => false
+
+// TEST CODE - Arrays As Values
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
