79b6af41883847c1d13d327348592178c4c69fd1
pochiman <pochiman@yahoo.com>
2021-11-22 17:12:53 -0500

    initial commit

---
+++ test/eqArraysTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
