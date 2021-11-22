a6a3ba579db494eb33111dec08a0de9e2e60c241
pochiman <pochiman@yahoo.com>
2021-11-22 17:56:18 -0500

    initial commit

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i] && (!Array.isArray(array1[i]) || !Array.isArray(array2[i]))) {
-      return false;
-    }
-    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
-      if (!eqArrays(array1[i], array2[i])) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-module.exports = eqArrays;
-
+const eqArrays = require('./eqArrays');
 
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(array1, array2) {
