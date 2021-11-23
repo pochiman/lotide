2180718382d6453e3e2d77623fc0b757d78f8e26
pochiman <pochiman@yahoo.com>
2021-11-22 21:52:05 -0500

    initial commit

--- test/middleTest.js
+++ test/middleTest.js
@@ -3,18 +3,18 @@ const assert = require('chai').assert;
 
 describe("#middle", () => {
 
-  it('returns [3, 4] for [1,2,3,4,5,6]', () => {
-    assert.deepEqual(middle([1,2,3,4,5,6]), [3, 4]);
-  });
-
   it('returns [3] for [1,2,3,4,5]', () => {
     assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+
+  it('returns [3, 4] for [1,2,3,4,5,6]', () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]), [3, 4]);
   });
   
   it('returns [] for ["batman", "robin"]', () => {
     assert.deepEqual(middle(["batman", "robin"]), []);
   });
-  
+
 });
 
 
