e801d4ff52516b682319d56d688d733b11663ca6
pochiman <pochiman@yahoo.com>
2021-11-22 19:57:05 -0500

    initial commit

--- middle.js
+++ middle.js
@@ -1,36 +1,5 @@
-// FUNCTION IMPLEMENTATION
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i] && (!Array.isArray(array1[i]) || !Array.isArray(array2[i]))) {
-      return false;
-    }
-    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
-      if (!eqArrays(array1[i], array2[i])) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-module.exports = eqArrays;
-
-
-// FUNCTION IMPLEMENTATION
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`👍 Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`👎 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
-
-module.exports = assertArraysEqual;
-
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 // FUNCTION IMPLEMENTATION
 const middle = function(array) {
@@ -44,13 +13,3 @@ const middle = function(array) {
 };
 
 module.exports = middle;
-
-// TEST CODE
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
