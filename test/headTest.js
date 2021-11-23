dfacd2d4f8b42b32be4153f4a0bea42ccf0a6f41
pochiman <pochiman@yahoo.com>
2021-11-22 21:50:38 -0500

    initial commit

--- test/headTest.js
+++ test/headTest.js
@@ -25,6 +25,6 @@ describe("#head", () => {
 // const assertEqual = require('../assertEqual');
 // const head = require('../head');
 
-// TEST CODE
+// // TEST CODE
 // assertEqual(head([5,6,7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,33 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+const middle = require('../middle')
+const assert = require('chai').assert;
 
-// TEST CODE
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
+describe("#middle", () => {
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
+  it('returns [3, 4] for [1,2,3,4,5,6]', () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]), [3, 4]);
+  });
 
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+  it('returns [3] for [1,2,3,4,5]', () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+  
+  it('returns [] for ["batman", "robin"]', () => {
+    assert.deepEqual(middle(["batman", "robin"]), []);
+  });
+  
+});
+
+
+// const eqArrays = require('../eqArrays');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
+
+// // TEST CODE
+// console.log(middle([1])); // => []
+// console.log(middle([1, 2])); // => []
+
+// console.log(middle([1, 2, 3])); // => [2]
+// console.log(middle([1, 2, 3, 4, 5])); // => [3]
+
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
