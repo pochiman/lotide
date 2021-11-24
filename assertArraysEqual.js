dd4825ec702a3019861a85e0790caee121826d1e
pochiman <pochiman@yahoo.com>
2021-11-23 23:24:08 -0500

    refactored, modularized, improved code readability

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 const eqArrays = require('./eqArrays');
 
-// FUNCTION IMPLEMENTATION
+// FUNCTION IMPLEMENTATION: takes in 2 arrays and returns true if they are equal and false otherwise
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`👍 Assertion Passed: ${array1} === ${array2}`);
