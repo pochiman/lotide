08551a517b7ae609f714f6d667c3ae8ca9165ca1
pochiman <pochiman@yahoo.com>
2021-11-24 00:17:08 -0500

    refactored to require assertEqual and eqArrays

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,7 @@
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
-// TEST CODE
+// TEST CODE 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
 
